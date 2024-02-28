8ccf4126dce6b09c6a0b746e8a7a6b51353dea6e
naman.gupta2 <naman.gupta2@sprinklr.com>
2024-02-28 13:05:47 +0530

    React learning commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import PostForm from './ components/PostForm';
+import HookCounter from './ components/HookCounter';
+import HookCounterFour from './ components/HookCounterFour';
+import CounterThree from './ components/CounterThree';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <CounterTwo /> */}
+      <CounterThree />
     </div>
   );
 }
